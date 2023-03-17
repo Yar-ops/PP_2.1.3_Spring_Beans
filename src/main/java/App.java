ee3bb97872e9accf9719ec2f4668b06059d05c1c
Yar Kon <akela199393@mail.ru>
2023-03-17 17:46:41 +0300

    Version 1.1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,8 +3,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
         HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
